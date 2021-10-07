1dc6039d740d016bacdc8641fd0c95069d288b2e
ArthurMaverick <arthuracs18@gmail.com>
2021-10-07 11:21:29 -0300

    some bugs fixed

--- api/src/main/config/expressMiddles.ts
+++ api/src/main/config/expressMiddles.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 
 export const setupMiddlewares = (app: Express): void  => {
   app.use((req,res,next)=> {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://ezops-test-arthursantos.vercel.app/");
     res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "*");
     next();
