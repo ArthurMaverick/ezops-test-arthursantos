60d7113d7f4b4852191a9c97c66ba8c98b6b1920
ArthurMaverick <arthuracs18@gmail.com>
2021-10-07 11:00:02 -0300

    upgrade some bugs

--- api/src/main/config/expressMiddles.ts
+++ api/src/main/config/expressMiddles.ts
@@ -2,13 +2,13 @@ import  {Express, json, Request, Response, NextFunction} from 'express'
 import cors from 'cors'
 
 export const setupMiddlewares = (app: Express): void  => {
-  // app.use((req,res,next)=> {
-  //   res.header("Access-Control-Allow-Origin", "*");
-  //   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  //   res.header("Access-Control-Allow-Headers", "X-PINGOTHER, content-type, Authorization");
-//   next();
-// })
-  app.use(cors())
+  app.use((req,res,next)=> {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, content-type, Authorization");
+    app.use(cors())
+  next();
+})
   app.use(json())
   app.use((req: Request, res: Response, next: NextFunction) => {
     res.type('json')
