ae2032a98b096dc02d2596af6c966a2c2f39b8ec
ArthurMaverick <arthuracs18@gmail.com>
2021-10-07 11:06:03 -0300

    some bugs fixed

--- api/src/main/config/expressMiddles.ts
+++ api/src/main/config/expressMiddles.ts
@@ -4,11 +4,11 @@ import cors from 'cors'
 export const setupMiddlewares = (app: Express): void  => {
   app.use((req,res,next)=> {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, content-type, Authorization");
-    app.use(cors())
-  next();
-})
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  })
+  app.use(cors())
   app.use(json())
   app.use((req: Request, res: Response, next: NextFunction) => {
     res.type('json')
