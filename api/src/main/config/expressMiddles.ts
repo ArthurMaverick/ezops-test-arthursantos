168b425873f798abe4ff7ab63cd0efe24f5a9bb2
ArthurMaverick <arthuracs18@gmail.com>
2021-10-07 11:12:01 -0300

    some bugs fixed

--- api/src/main/config/expressMiddles.ts
+++ api/src/main/config/expressMiddles.ts
@@ -7,8 +7,8 @@ export const setupMiddlewares = (app: Express): void  => {
     res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "*");
     next();
+    app.use(cors())
   })
-  app.use(cors())
   app.use(json())
   app.use((req: Request, res: Response, next: NextFunction) => {
     res.type('json')
