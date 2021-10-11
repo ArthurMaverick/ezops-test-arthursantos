2b8192cbc902f9477e7503a1bb796e8f502fd8e2
ArthurMaverick <arthuracs18@gmail.com>
2021-10-11 20:42:39 -0300

    upgrade

--- api/src/main/config/env.ts
+++ api/src/main/config/env.ts
@@ -1,6 +1,6 @@
 require('dotenv').config()
 export const env = {
-  port: process.env.PORT ?? 8080,
+  port: process.env.PORT ?? 7000,
   jwtSecret: process.env.JWT_SECRET ?? 'chavesupersecreta',
   mongoDB: process.env.MONGO_URL,
   mongoDBdev: process.env.MONGO_URL_DEV
